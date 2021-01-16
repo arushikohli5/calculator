376fa5760b52ced7cdf19bd68f364d73897a47f9
Arushi Kohli <arushi1467.cse18@chitkara.edu.in>
2021-01-16 22:48:45 +0530

    js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+class calculator{
+    constructor(prevButtons,currentButtons){
+        this.prevButtons=prevButtons;
+        this.currentButtons=currentButtons;
+        this.clear();
+    }
+
+    clear(){
+        this.currentOperand='';
+        this.prevOperand='';
+        this.operation=undefined;
+    }
+
+    delete(){}
+
+    chooseOperation(operation){}
+
+    number(number){}
+
+    compute(){}
+
+    updateDisplay(){}
+}
 const numberButtons=document.querySelectorAll('[data-number]');
 const operationButtons=document.querySelectorAll('[data-operation]');
 const equalButtons=document.querySelectorAll('[data-equals]');
@@ -5,3 +28,14 @@ const deleteButtons=document.querySelectorAll('[data-delete]');
 const allClearButtons=document.querySelectorAll('[data-ac]');
 const prevButtons=document.querySelectorAll('[data-prev]');
 const currentButtons=document.querySelectorAll('[data-current]');
+
+const claculator=new calculator(prevButtons,currentButtons);
+
+
+
+
+
+
+
+
+
