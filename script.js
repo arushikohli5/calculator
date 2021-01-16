3aaec832d53d6b612fc51e960be0f4720a981cd0
Arushi Kohli <arushi1467.cse18@chitkara.edu.in>
2021-01-16 22:39:17 +0530

    js initial

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons=document.querySelectorAll('[data-number]');
+const operationButtons=document.querySelectorAll('[data-operation]');
+const equalButtons=document.querySelectorAll('[data-equals]');
+const deleteButtons=document.querySelectorAll('[data-delete]');
+const allClearButtons=document.querySelectorAll('[data-ac]');
+const prevButtons=document.querySelectorAll('[data-prev]');
+const currentButtons=document.querySelectorAll('[data-current]');
